1910bbd93966f261fb9a8a135074a7fed56a3fec
KaiqueSousa <kaique.una@gmail.com>
2018-11-19 10:36:58 -0300

    Dockerfile banco

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-docker build -t ricardojob/banco ./postgres
-docker run -p 5433:5432  --name banco  -d ricardojob/banco 
+docker build -t atividade/banco ./postgres
+docker run -p 5433:5432  --name banco  -d atividade/banco 
 mvn clean package
-docker build -t ricardojob/app .
-docker run -p 8082:8080 --name app --link banco:host-banco -d  -v $(pwd)/apps:/usr/local/tomcat/testes ricardojob/app 
+docker build -t atividade/app .
+docker run -p 8082:8080 --name app --link banco:host-banco -d atividade/app 
 

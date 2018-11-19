2f022f79350cb3e8baae90ace37f6ffd799de58d
MurilloTavares <murillo_eum@hotmail.com>
2018-11-19 10:58:49 -0300

    Add CPFConverter

---
+++ src/main/java/br/edu/ifpb/pratica/converter/CPFConverter.java
@@ -0,0 +1,29 @@
+package br.edu.ifpb.pratica.converter;
+
+import br.edu.ifpb.pratica.model.CPF;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("converter.CPF")
+public class CPFConverter implements Converter{
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null) {
+            return null;
+        }
+        return new CPF(value);
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (value == null) {
+            return null;
+        }
+        CPF cpf = (CPF) value;
+        return cpf.numero();
+    }
+    
+}
